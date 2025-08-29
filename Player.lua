7a8cbcd3f356e97edba8b1e2b162f6765882f3a9
Achim Kuntz <achimobil@hotmail.com>
2025-08-29 21:02:05 +0200

    Update Player.lua
    Player getPosition hinzgefügt

--- Player.lua
+++ Player.lua
@@ -1,3 +1,22 @@
 Player = {};
 
+local Player_mt = Class(Player, Object)
+
+--- create new instance
+-- @param boolean isServer
+-- @param boolean isClient
+-- @return ProductionPoint newObject
+function Player.new(isServer, isClient)
+    local self = Object.new(isServer, isClient, Player_mt);
+    return self;
+end
+
+--- Get Position of the player
+-- @return float x
+-- @return float y
+-- @return float z
+function Player:getPosition()
+    return 0,0,0;
+end
+
 g_localPlayer = Player;
